dc21c440cce5974bf09ed5c95ad7a40c37eb02d5
twingay96 <flavonoid37@gmail.com>
2023-12-04 20:30:53 +0900

    add messages to the list(by turbo_stream)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   # GET /messages or /messages.json
   def index
-    @messages = Message.all
+    @messages = Message.order(created_at: :desc) # asc: 오름차순 
   end
 
   # GET /messages/1 or /messages/1.json
@@ -25,9 +25,27 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update('new_message', partial: "messages/form" ,locals: {message: Message.new}),
+            # update : 특정 요소를 바꿈
+            # new_message -> 터보스트림으로 바꿀 html요소(div id="new_message") , partial -> 바꾼 html 요소안에 넣을 템플릿 , locals-> 템플릿에 전달할 로컬변수(비어있는 폼)
+            turbo_stream.prepend('messages', partial: "messages/message" ,locals: {message: @message}),
+            # prepend : 특정 요소의 맨위에 추가
+            # messages-> index 페이지의 html요소(div id="messages"), partial-> 추가할 위치에 넣을 템플릿, locals-> 템플릿에 전달할 변수(submit한객체)
+          ]
+        end
         format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
         format.json { render :show, status: :created, location: @message }
       else
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update('new_message', 
+                                  partial: "messages/form" ,
+                                  locals: {message: @message})
+            # element_id -> 터보스트림으로 바꿀 html요소 , partial -> 바꾼 html 요소안에 넣을 템플릿 , locals-> 템플릿에 전달할 로컬변수
+          ]
+        end
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
